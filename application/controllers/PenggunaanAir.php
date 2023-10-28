8bccf152e6133c9a82a1feb2e9155e2da82db44d
bgsmhrdka_ <bagasmahardikabudi2007@gmail.com>
2023-10-28 19:12:53 +0700

    update

--- application/controllers/PenggunaanAir.php
+++ application/controllers/PenggunaanAir.php
@@ -13,6 +13,32 @@ class PenggunaanAir extends CI_Controller
         $this->load->model('Pelanggan_model');
     }
 
+    public function index()
+    {
+        $data = [
+            'title' => 'Penggunaan Air',
+            'bulan' => ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
+            'user' => $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array(),
+            'pelangan' => $this->db->get('pelanggan')->result_array()
+        ];
+
+        $this->form_validation->set_rules('pelanggan_id', 'Id Pelanggan', 'trim|required|numeric');
+        $this->form_validation->set_rules('pemakaian_awal', 'Pemakaian Awal', 'trim|required|numeric');
+        $this->form_validation->set_rules('pemakaian_akhir', 'Pemakaian Akhir', 'trim|required|numeric');
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('layout/header', $data);
+            $this->load->view('layout/sidebar', $data);
+            $this->load->view('layout/navbar', $data);
+            $this->load->view('penggunaanAir/index', $data);
+            $this->load->view('layout/footer');
+        } else {
+            $this->PenggunaanAir_model->tambahDataPenggunaanAir();
+            $this->session->set_flashdata('flash', 'Berhasil DiTambah');
+            redirect('penggunaanair/belumBayar');
+        }
+    }
+
     public function input()
     {
         $data = [
